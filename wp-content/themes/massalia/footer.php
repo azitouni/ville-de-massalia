a7449ff775f9d864f82fcb5edde42386c2db5823
azitouni <zitouniadel@hotmail.com>
2016-12-01 09:48:38 +0100

    1st commit

---
+++ wp-content/themes/massalia/footer.php
@@ -0,0 +1,24 @@
+</main>
+<div id="wrapper-footer" class="center">
+	<footer id="footer" class="max-largeur center" role="contentinfo">
+		<div class="grid grid-2 grid-center">
+
+			<address class="identite">
+				<?php dynamic_sidebar('coordonnees') ?>
+			</address>
+			<?php
+			wp_nav_menu(
+				array(
+					'container'=>false,
+					'menu_class'=>'menu-hz',
+					'menu'=>'Pied de page'
+				)
+			);
+			?>
+		</div>
+	</footer>
+</div>
+<?php wp_footer(); 
+?>
+</body>
+</html>
